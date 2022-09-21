7729e0bdd5088bab6693a520a70fa28060603871
dewdrops77 <okojieleonnie8@gmail.com>
2022-09-21 17:20:48 +0000

    print

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for b in range(ord('z'), ord('a') - 1, -2):
-    print("{:c}{:s}".format(b, chr(b - 33)), end="")
+for i in reversed(range(97, 123)):
+    if (i % 2 == 0):
+        print('{:c}'.format(i), end='')
+    else:
+        print('{:c}'.format(i - 32), end='')

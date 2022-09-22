b9ee5cffe917d5d4e183131a91142dce0b7719e5
dewdrops77 <okojieleonnie8@gmail.com>
2022-09-22 21:58:40 +0000

    fix

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,7 +2,12 @@
 from calculator_1 import add, sub, mul, div
 
 if __name__ == '__main__':
-  
+    """
+
+    Prints the result of the addition, substract, multiplication
+    and division between two numbers
+
+    """
     a = 10
     b = 5
     print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))

41d553f5e73c6fb2dd4d913d8fb5d61d1cb39c8a
dewdrops77 <okojieleonnie8@gmail.com>
2022-09-24 16:18:27 +0000

    1-element_at.py

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < 0:
+       return("None")
+    if i is out of range(5):
+       return("None")
+       print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))

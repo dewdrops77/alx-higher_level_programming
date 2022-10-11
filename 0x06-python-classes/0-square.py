37ada94dfb332050dcdddfd24b3bd027d5f198b7
dewdrops77 <okojieleonnie8@gmail.com>
2022-10-11 17:38:46 +0000

    the Python class MagicClass that does exactly the same as the following Python bytecode

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-# 0-square.py
-# Brennan D Baraban <375@holbertonschool.com>
 """Define a class Square."""
 
-
 class Square:
-    """Represent a square."""
-    pass
+"""Represent a square."""

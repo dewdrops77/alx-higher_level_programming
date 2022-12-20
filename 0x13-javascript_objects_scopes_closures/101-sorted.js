3c9ca1ba2ce3a18ee38b38b8fe0739873693d458
dewdrops77 <okojieleonnie8@gmail.com>
2022-12-20 20:12:41 +0000

    101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-const dict = require('./101-data.js').dict;
-let newDict = {};
-for (let key in dict) {
-  if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] = [key];
-  } else {
-    newDict[dict[key]].push(key);
+const dict = require('./101-data').dict;
+const nDict = {};
+for (const key in dict) {
+  if (nDict[dict[key]] === undefined) {
+    nDict[dict[key]] = [];
   }
+  nDict[dict[key]].push(key);
 }
-console.log(newDict);
+console.log(nDict);

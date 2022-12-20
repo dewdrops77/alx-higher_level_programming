94342efbba8e9bb9416905ffd0105559e3466d5f
dewdrops77 <okojieleonnie8@gmail.com>
2022-12-20 18:14:29 +0000

    7-model_state_fetch_all.sql

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,19 +1,35 @@
 #!/usr/bin/python3
-# Displays all cities of a given state from the
-# states table of the database hbtn_0e_4_usa.
-# Safe from SQL injections.
-# Usage: ./5-filter_cities.py <mysql username> \
-#                             <mysql password> \
-#                             <database name> \
-#                             <state name searched>
+"""
+ takes in the name of a state as an argument
+ and lists all cities of that state, using the
+database hbtn_0e_4_usa
+"""
 import sys
 import MySQLdb
 
+
+def main():
+    conn = MySQLdb.connect(
+                        host="localhost",
+                        port=3306,
+                        user=sys.argv[1],
+                        passwd=sys.argv[2],
+                        db=sys.argv[3],
+                        charset="utf8"
+                            )
+    state_name = sys.argv[4]
+    cur = conn.cursor()
+    cur.execute("SELECT cities.name FROM cities \
+    JOIN states ON cities.state_id = states.id WHERE states.name LIKE %s \
+    ORDER BY cities.id", (state_name,))
+    row = cur.fetchall()
+    re = ""
+    for r in row:
+        re += r[0] + ", "
+    print(re[0:-2:])
+    cur.close()
+    conn.close()
+
+
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `cities` as `c` \
-                INNER JOIN `states` as `s` \
-                   ON `c`.`state_id` = `s`.`id` \
-                ORDER BY `c`.`id`")
-    print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
+    main()
